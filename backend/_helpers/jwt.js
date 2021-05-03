523eb4091cabc5e26a8e9d381a2f1b4752a429c4
James Park <jamespark0823@vt.edu>
2021-05-03 01:36:38 -0400

    gallery + limit fix

--- backend/_helpers/jwt.js
+++ backend/_helpers/jwt.js
@@ -7,7 +7,7 @@ module.exports = jwt;
 function jwt(){
     const secret = config.secret;
     return new expressJwt({secret, getToken: req => req.cookies.token, isRevoked, algorithms: ['HS256']}).unless({
-        path: /^\/(post\/(?!(new|like|repost))(.*|show\/.*))|\/(user\/(register|authenticate))|\/login|\/$/
+        path: /^\/(post\/(?!(new|like|repost|addtogallery))(.*|show\/.*))|\/(user\/(register|authenticate))|\/login|\/$/
     });
 }
 
