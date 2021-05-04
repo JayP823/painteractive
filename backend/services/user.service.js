5f684b4bfb146977bb49fc9ff4a30b8c709b22a5
James Park <jamespark0823@vt.edu>
2021-05-04 04:13:59 -0400

    fixed gallery

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -149,7 +149,7 @@ async function gallery(req){
     let skipNum = req.query.page * 12;
     let user = await User.find({username: req.query.username});
     let user2 = user[0];
-    return await User.find({username: user2._id}, {gallery: 1}).sort({createdDate: -1}).populate('image').skip(skipNum).limit(12);
+    return await User.find({_id: user2._id}, {gallery: 1}).sort({createdDate: -1}).populate('image').skip(skipNum).limit(12);
 }
 
 async function follow(req, res){
