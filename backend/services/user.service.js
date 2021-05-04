b41ff703f5a9f6b016f4575633d6e8e51cc3312d
James Park <jamespark0823@vt.edu>
2021-05-04 07:05:15 -0400

    populate

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -128,6 +128,8 @@ async function getUserPosts(req){
     let skipNum = req.query.page * 12;
     let user = await User.find({username: req.query.username});
     let user2 = user[0];
+    let post = await Post.find({$or: [{createdBy: user2._id}, {reposted: user2._id}]}).sort({createdDate: -1}).skip(skipNum).limit(12).populate({path:'createdBy'}).populate({path: 'comments.user'}).populate('image');
+    console.log(post[0].createdBy)
     return await Post.find({$or: [{createdBy: user2._id}, {reposted: user2._id}]}).sort({createdDate: -1}).skip(skipNum).limit(12).populate({path:'createdBy'}).populate({path: 'comments.user'}).populate('image');
 }
 
